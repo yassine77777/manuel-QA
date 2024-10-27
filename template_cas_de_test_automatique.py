5f3d9d04c5a303299da293ba3ec9a0cbc40b9c3f
yassine77777 <yassineakrout111@gmail.com>
2024-10-27 13:30:55 +0100

    test automatique

---
+++ template_cas_de_test_automatique.py
@@ -0,0 +1,26 @@
+﻿from time import sleep,time
+
+class TCa_01_Test_Authentification:
+    REQ_ID = "Req-10"
+    STATUS = "Ready"
+    
+    def prepare(self):
+        self.start_Connexion()
+		self.open_site("www.erp.com")
+        self.create_account()
+        
+    def execute(self):
+        self.enter_username("Mohamed")
+        sleep(3)
+        self.check_username_entred("Mohamed")
+        self.enter_password("1234")
+        sleep(3)
+        self.check_password_entred("1234")
+		self.clik_button("sign in")
+		self.check_access_to_account()
+        
+    def clean_up(self):
+		self.disonnect_from_account()
+        self.close_site("www.erp.com")          
+        self.clear_all_msg_senders() 
+        
